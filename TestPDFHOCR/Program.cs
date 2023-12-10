d2fdd84000ef7489d1c25d3ff85bee0c1e2db99a
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2023-12-10 21:58:22 +0200

    delete image

--- TestPDFHOCR/Program.cs
+++ TestPDFHOCR/Program.cs
@@ -5,67 +5,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        new License().SetLicense("/home/tigra/test.lic");
-        
-        var pdfDocument = new Document("/home/tigra/MD_TEST.pdf");
-        
+        new License().SetLicense("./test.lic");
+
+        var pdfDocument = new Document("./test.pdf");
+
         Document.CallBackGetHocr recognizeText = (Aspose.Pdf.Drawing.PdfImage img) =>
         {
-            string outputFolder="/home/tigra/";
-            string tmpFile = "/home/tigra/789.jpg";//Path.Combine(outputFolder, Path.GetFileName(Path.GetTempFileName()));
+            string outputFolder = "./";
+            string tmpFile = "./789.jpg";//Path.Combine(outputFolder, Path.GetFileName(Path.GetTempFileName()));
             tmpFile = Path.ChangeExtension(tmpFile, "jpg");
-            try
+            using (var fs = File.Create(tmpFile))
             {
-                using (var fs = File.Create(tmpFile))
-                {
-                    img.BitmapStream.Seek(0, SeekOrigin.Begin);
-                    img.BitmapStream.CopyTo(fs);
-                }
-
-                string pathTempFile = $"\"{tmpFile}\"";
-                string arguments = $"{pathTempFile} {pathTempFile} --oem 1 -l eng hocr";
-                string tesseractProcessName = @"tesseract";
-
-                System.Diagnostics.ProcessStartInfo psi =
-                    new System.Diagnostics.ProcessStartInfo(tesseractProcessName, arguments);
-
-                using (System.Diagnostics.Process p = new System.Diagnostics.Process())
-                {
-                    p.StartInfo = psi;
-                    p.Start();
-                    p.PriorityClass = System.Diagnostics.ProcessPriorityClass.BelowNormal;
-                    p.WaitForExit();
-                }
-
-                return File.ReadAllText($"{tmpFile}.hocr");
+                img.BitmapStream.Seek(0, SeekOrigin.Begin);
+                img.BitmapStream.CopyTo(fs);
             }
-            catch (Exception)
+
+            string pathTempFile = $"\"{tmpFile}\"";
+            string arguments = $"{pathTempFile} {pathTempFile} --oem 1 -l eng hocr";
+            string tesseractProcessName = @"tesseract";
+
+            System.Diagnostics.ProcessStartInfo psi =
+                new System.Diagnostics.ProcessStartInfo(tesseractProcessName, arguments);
+
+            using (System.Diagnostics.Process p = new System.Diagnostics.Process())
             {
-                return string.Empty;
+                p.StartInfo = psi;
+                p.Start();
+                p.PriorityClass = System.Diagnostics.ProcessPriorityClass.BelowNormal;
+                p.WaitForExit();
             }
-            finally
-            {
-                if (File.Exists(tmpFile))
-                    File.Delete(tmpFile);
-                if (File.Exists(tmpFile + ".hocr"))
-                    File.Delete(tmpFile + ".hocr");
-            }
+
+            return File.ReadAllText($"{tmpFile}.hocr");
         };
-        
+
         bool result = pdfDocument.Convert(recognizeText);
-        
-        Console.WriteLine(result);
-        
+        //var page = pdfDocument.Pages[1];
+        //page.Resources.Images.Replace(1, File.OpenRead("./blank.png"));
+        pdfDocument.Save("./searchable_test.pdf");
+        pdfDocument.Dispose();
+
+        var doc2 = new Document("./searchable_test.pdf");
+        doc2.Pages[1].Resources.Images.Delete(1);
+        doc2.Save("./searchable_test.pdf");
+
+
         //Console.WriteLine(pdfDocument.Pages[1].Resources.Images.Count);
-        
-        pdfDocument.Pages[1].Resources.Images.Delete(1);
+
+        //pdfDocument.Pages[1].Resources.Images.Delete(1);
         /*
         ParagraphAbsorber absorb = new ParagraphAbsorber();
         absorb.Visit(pdfDocument);
 
         var pdfNewDoc = new Document();
         Page page = pdfNewDoc.Pages.Add();
-        
+
         foreach (PageMarkup markup in absorb.PageMarkups)
         {
             foreach (MarkupSection section in markup.Sections)
@@ -85,10 +78,9 @@ class Program
                     }
                 }
             }
-        }                
+        }
 
         pdfNewDoc.Save("/home/tigra/MD_TEST_NEW.pdf");*/
-        
-        pdfDocument.Save("/home/tigra/MD_TEST_PDF.pdf");
+
     }
 }
